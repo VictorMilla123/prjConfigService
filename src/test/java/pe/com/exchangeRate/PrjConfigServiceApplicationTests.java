2c6deb8f081cf8db6d24045b4afecc9f67ce7de2
unknown <vmillamora@gmail.com>
2023-11-28 00:38:13 -0500

    comitenado proj-config-service

---
+++ src/test/java/pe/com/exchangeRate/PrjConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.exchangeRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
